ef0dc41766e32a3d6ecc1cb2310af2a4d9dbe62f
Shebin John <admin@remedcu.com>
2018-08-28 13:40:13 +0530

    28th August 2018

---
+++ 30 Days of Code/Day 14 Scope.py
@@ -0,0 +1,6 @@
+#Answer to Day 14: Scope
+
+    def computeDifference(self):
+        self.maximumDifference=max(self.__elements)-min(self.__elements)
+        
+#The above indentation is because we are continuing the class given in the question
