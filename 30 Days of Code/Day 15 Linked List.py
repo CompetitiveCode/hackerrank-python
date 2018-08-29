518d879cd8d7fbe377dfc91926f1b200b3c29b8e
Shebin John <admin@remedcu.com>
2018-08-29 20:42:34 +0530

    29th August 2018

---
+++ 30 Days of Code/Day 15 Linked List.py
@@ -0,0 +1,6 @@
+#Answer to Day 15: Linked List
+
+    def insert(self,head,data):
+        print(data, end=" ")
+        
+#This is not the right way to solve this. I would advice you to spend some more time on the concept of Linked List, as this is really important for CS Students or for beginners.
