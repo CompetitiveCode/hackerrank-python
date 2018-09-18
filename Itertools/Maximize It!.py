59e90cb822aabeda9c79d7ad9a6dfee4d81341b5
Shebin John <admin@remedcu.com>
2018-09-18 16:56:00 +0530

    18th September 2018

---
+++ Itertools/Maximize It!.py
@@ -0,0 +1,7 @@
+#Answer to Maximize It! using external help
+
+from itertools import product
+K,M = map(int,input().split())
+N = (list(map(int, input().split()))[1:] for _ in range(K))
+results = map(lambda x: sum(i**2 for i in x)%M, product(*N))
+print(max(results))
