49e69d7bb08cc4f9d5e19338885a06ac2ba0d66c
Shebin John <admin@remedcu.com>
2019-03-23 20:30:48 +0530

    Added HackerRank Warmup Algorithms

---
+++ Practice/Algorithms/Warmup/Compare the Triplets.py
@@ -0,0 +1,31 @@
+#Answer to Compare the Triplets - https://www.hackerrank.com/challenges/compare-the-triplets/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    k,l = 0,0
+    for i,j in zip(a,b):
+        if i > j: k += 1
+        elif i < j: l += 1
+    return [k,l]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
